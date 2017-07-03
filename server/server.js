be4d01b7f168803da2b68732cfabb80b1b820dcc
Luca Guarro <luca.guarro@gmail.com>
2017-07-03 14:39:52 -0700

    GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser'); //bodyParser takes our JSON and converts it into an object.
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -26,10 +27,33 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => { //our get request to get all todos. Will make it so that you can get individual todos as well.
     Todo.find().then((todos) => {
-        res.send({todos});
+        res.send({todos}); //data sent
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+// GET /todos/1234324
+app.get('/todos/:id', (req, res) => { //to make a dynamic array we add a parameter noted by ": + name" = ":id". We still get the request and response objects.
+    //req.params  //Now we actually use the request object to access params which is an object that has key-value pairs where the key is the URL parameter
+                //like id and the value is what was actually put there 
+    var id = req.params.id;
+
+    //validate id using isValid
+        //404 if not valid - send back empty body
+    if(!ObjectID.isValid(id)){ //validating IDs
+       return res.status(404).send();
+    }
+    //findById
+    Todo.findById(id).then((todo) => { //success
+        if(!todo){ //if no todo - send back 404 with empty body
+            return res.status(404).send();
+        }
+        res.send({todo}); //if todo - send it back
+    }).catch((e) => { //error
+        res.status(400).send(); //400 request was not valid - and send empty body back
+    });
+            
 });
 
 app.listen(3000, () => {
