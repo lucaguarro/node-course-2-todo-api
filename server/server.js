095dd5f645235e6455eebddd1d81b31c0cb9d2d3
Luca Guarro <luca.guarro@gmail.com>
2017-07-03 12:10:24 -0700

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -24,6 +24,14 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => { //our get request to get all todos. Will make it so that you can get individual todos as well.
+    Todo.find().then((todos) => {
+        res.send({todos})
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
